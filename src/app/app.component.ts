db9dda7f5998e501172fddf818c31e142624f94f
aymen bennour <aymenokay@gmail.com>
2018-10-07 02:05:50 +0100

    finish the numbers buttons setups
    number are printing to the screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tp1-calculator';
+  science_flag: boolean;
+  result: string;
+  constructor() {
+    this.science_flag = false;
+    this.result = '0';
+  }
+  printScreen(str: string) {
+    if (this.result === '0') {this.result = str; }
+    else {
+    this.result = this.result + str; }
+  }
+  toggleVisibility() {
+    this.science_flag = !this.science_flag;
+  }
 }
